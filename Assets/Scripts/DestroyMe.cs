d38967b4c65237412dc2a24f8b700831bc8d6aac
Teemu Salminen <teemujsa@gmail.com>
2019-10-17 14:17:02 +0300

    Add destroyMe and moveMe scripts

---
+++ Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMe : MonoBehaviour
+{
+    public Collider Cube;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == Cube)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        TryRaycast();
+    }
+
+    private void TryRaycast()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            bool raycastSuccesful = Physics.Raycast(ray, out raycastHit);
+            if(raycastSuccesful)
+            {
+                if(raycastHit.transform == transform)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveMe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour
+{
+    private bool isDragging = false;
+    private float distanceToCamera;
+
+    void Update()
+    {
+        TryMove();
+    }
+
+    private void TryMove()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            bool raycastSuccesful = Physics.Raycast(ray, out raycastHit);
+            if (raycastSuccesful)
+            {
+                if (raycastHit.transform == transform)
+                {
+                    isDragging = true;
+                    distanceToCamera = (transform.position - Camera.main.transform.position).magnitude;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1) && isDragging)
+            isDragging = false;
+
+        if(isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 newPosition = ray.GetPoint(distanceToCamera);
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+        }
+    }
+}
